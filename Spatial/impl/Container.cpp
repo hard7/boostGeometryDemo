188f90a19ba25e7a629b29e00f7114308ce691e7
Alexandr Nosov <anosov@ivc.nnov.ru>
2016-04-14 11:52:38 +0400

    make rtree

--- Spatial/impl/Container.cpp
+++ Spatial/impl/Container.cpp
@@ -3,22 +3,43 @@
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/register/point.hpp>
 #include <boost/geometry/geometries/register/box.hpp>
+#include <boost/range/adaptor/indexed.hpp>
+#include <boost/range/adaptor/transformed.hpp>
 
-namespace {
-    typedef Spatial::Component::Point Point;
-    typedef Spatial::Component::Box Box;
-}
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+
+typedef Spatial::Component::Point Point;
+typedef Spatial::Component::Box Box;
 
 BOOST_GEOMETRY_REGISTER_POINT_3D_GET_SET(Point, Point::type, boost::geometry::cs::cartesian, x, y, z, x, y, z);
-BOOST_GEOMETRY_REGISTER_BOX(Box, Point, lo, hi);
+BOOST_GEOMETRY_REGISTER_BOX(Box, Point, lo(), hi());
+
+namespace {
+// Define a function object converting a value_type of indexed Range into std::pair<>.
+// This is a generic implementation but of course it'd be possible to use some
+// specific types. One could also take Value as template parameter and access
+// first_type and second_type members, etc.
+    template <typename First, typename Second>
+    struct pair_maker {
+        typedef std::pair<First, Second> result_type;
+        template<typename T>
+        inline result_type operator()(T const& v) const  {
+            return result_type(v.value(), v.index());
+        }
+    };
+}
 
 namespace Spatial {
 
 struct Container::Impl {
+    typedef std::pair<Box, int> Value;
+
     _implContainer container;
     const std::vector<Box> boxes;
+    bgi::rtree< Value, bgi::quadratic<20> > rtree;
 
-    Impl(Boxes const& boxes_) : boxes(boxes_) {
+    Impl(Boxes const& boxes_) : boxes(boxes_), rtree(boxes_ | boost::adaptors::indexed() | boost::adaptors::transformed(pair_maker<Box, int>())) {
 //        container.resize(boxes.size());
     }
 };
